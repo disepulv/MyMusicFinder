67c6ea88322d63e6062bba9155c7432fe7c5c42d
Diego Sepúlveda <disepulv@gmail.com>
2019-12-11 09:29:33 -0300

    set up base project

---
+++ MyMediaFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMediaFinder
+//
+//  Created by Diego on 12/11/19.
+//  Copyright © 2019 Diego Sepúlveda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
