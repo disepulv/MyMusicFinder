7ad26d8b64a2cbaba56fcb633b60bd0609c7ede3
Diego Sepúlveda <disepulv@gmail.com>
2019-12-11 12:35:26 -0300

    adds query validation

---
+++ MyMediaFinder/Helpers/String+Extensions.swift
@@ -0,0 +1,19 @@
+//
+//  String+Extensions.swift
+//  MyMediaFinder
+//
+//  Created by Diego on 12/11/19.
+//  Copyright © 2019 Diego Sepúlveda. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    func isValidQuery() -> Bool {
+        let queryRegEx = "^[A-Za-z0-9.-]{3,64}"
+
+        let queryPred = NSPredicate(format:"SELF MATCHES %@", queryRegEx)
+        return queryPred.evaluate(with: self)
+    }
+}
